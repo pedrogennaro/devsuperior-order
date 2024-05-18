4814dc4688ccbc0f667a79d55aec868a9efdd861
Pedro De Gennaro <pedrogennaro@gmail.com>
2024-05-18 10:52:19 -0300

    first commit

---
+++ src/main/java/com/pedrogennaro/devsuperiororder/DevsuperiorOrderApplication.java
@@ -0,0 +1,13 @@
+package com.pedrogennaro.devsuperiororder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperiorOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuperiorOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrogennaro/devsuperiororder/DevsuperiorOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrogennaro.devsuperiororder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
